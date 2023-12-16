f87f383e8d1231d14f98839a0fd56c84b19fa9ff
KAMLESH LODHI <kamleshlodhi9302@gmail.com>
2023-12-16 20:05:38 +0530

    Y

--- ..DB/database.sql
+++ ..DB/database.sql
@@ -4,15 +4,16 @@ USE library_db;
 
 -- Table for Admin
 CREATE TABLE IF NOT EXISTS librarian (
-  
+  id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(20) NOT NULL,
   password CHAR(40) NOT NULL,
   email VARCHAR(255) NOT NULL
 );
 
+
 -- Table for User
 CREATE TABLE IF NOT EXISTS member (
-  
+  id INT AUTO_INCREMENT PRIMARY KEY,
   Username VARCHAR(20) NOT NULL,
   password CHAR(40) NOT NULL,
   name VARCHAR(80) NOT NULL,
@@ -20,6 +21,7 @@ CREATE TABLE IF NOT EXISTS member (
   balance INT(4) NOT NULL,
   address VARCHAR(255) DEFAULT 'N/A'
 );
+
 
 -- Table for Book
 CREATE TABLE IF NOT EXISTS books (
@@ -31,22 +33,32 @@ CREATE TABLE IF NOT EXISTS books (
   copies INT(10) UNSIGNED NOT NULL
 );
 
+
+
 -- Table for Book Issue Log
 CREATE TABLE IF NOT EXISTS books_issue_log (
-  
+  issue_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   member_id INT(11) NOT NULL,
   book_isbn CHAR(13) NOT NULL,
   due_date DATE NOT NULL,
-  return_date DATE,
-  FOREIGN KEY (member_id) REFERENCES member (id),
-  FOREIGN KEY (book_isbn) REFERENCES books (isbn)
+  return_date DATE
 );
 
--- Table for Pending Registration
+
+-- 
 CREATE TABLE IF NOT EXISTS pending_registration (
-  
+  request_id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(20) NOT NULL,
   password CHAR(40) NOT NULL,
   email VARCHAR(80) NOT NULL,
   balance INT(4) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS pending_books (
+  isbn CHAR(13) NOT NULL PRIMARY KEY,
+  title VARCHAR(80) NOT NULL,
+  author VARCHAR(80) NOT NULL,
+  category VARCHAR(80) NOT NULL,
+  price INT(4) UNSIGNED NOT NULL
 );
